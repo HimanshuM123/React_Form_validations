4da4c572e96387f9a28c18eeb848b837476f1410
unknown <himanshu.mahale2010@gmail.comm>
2022-07-02 19:37:19 +0530

    header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./header.css";
+
+const Header = () => {
+  return (
+    <div className="menu">
+      <ul className="menuList">
+        <li className="listItem">Home</li>
+        <li className="listItem">About Us</li>
+        <li className="listItem">Contact Us</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Header;
